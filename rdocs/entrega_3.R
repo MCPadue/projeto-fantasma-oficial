829c6d0ea00ecc00137f4f6683c7246dc17be944
MCPadue <paduemcm@gmail.com>
2025-10-26 19:31:33 -0300

    .

--- rdocs/entrega_3.R
+++ rdocs/entrega_3.R
@@ -22,3 +22,63 @@ source("rdocs/source/packages.R")
 # de teste depreciados, ou ao menos deixando como comentário. Dê preferência
 # as funções dos pacotes contidos no Tidyverse para realizar suas análises.
 # ---------------------------------------------------------------------------- #
+
+## Teremos que trabalhar com CityID(Ambar Seco), Age:infos_clientes,StoreID:infos_lojas
+
+## Primeira coisa, vamos organizar os nomes
+rm(infos_lojas)
+rm(infos_cidades)
+infos_lojas<- read_excel("relatorio_old_town_road.xlsx", sheet = "infos_lojas")
+infos_cidades<- read_excel("relatorio_old_town_road.xlsx", sheet = "infos_cidades")
+
+infos_lojas<- infos_lojas %>%
+  rename(StoreID=Stor3ID)
+infos_cidades<-infos_cidades %>%
+  rename(CityID=C1tyID)
+
+
+
+
+
+## Criando uma nova tabela com infos_clientes,infos_lojas e infos_cidades
+## 1- selecionamos a tabela info_cidades e infos_lojas e usamos a coluna CityID,que é a coluna em comum entre elas, para juntá-las. 
+## 2- fazemos a mesma coisa com tabela_organizar e relatorio_vendas e tabela_organizar e infos_clientes
+## 3- criamos uma nova tabela, filtrando a tabela_organizar apenas para ambar seco, a partir de filter.
+
+tabela_organizar<- inner_join(infos_cidades,infos_lojas,by="CityID")
+
+tabela_organizar<-inner_join(tabela_organizar,relatorio_vendas,by="StoreID")
+tabela_organizar<-inner_join(tabela_organizar,infos_clientes,by="ClientID")
+
+tabela_organizar_filtrada <- tabela_organizar %>%
+  filter(NameCity == "Âmbar Seco")
+
+
+
+
+
+  
+
+
+## O gráfico mais adequado será um boxplot bivariado, pois queremos saber omo as idades dos clientes variam dependendo da loja.
+
+install.packages("ggplot2")
+library(ggplot2)
+library(tidyverse)
+
+grafico_ambarseco<-ggplot(tabela_organizar_filtrada, aes(x =reorder(NameStore,Age,Fun=median) , y = Age)) +
+  geom_boxplot(fill = "#A11D21", width = 0.7) +
+  stat_summary(
+    fun = "mean", geom = "point", shape = 23, size = 3, fill = "grey"
+  ) +
+  labs(
+    x = "Loja da Cidade Âmbar Seco",
+    y = "Idade do Cliente",
+    title = "Perfis das Idades dos Clientes por Loja — Âmbar Seco"
+  ) +
+  theme_estat()
+
+
+
+
+  
